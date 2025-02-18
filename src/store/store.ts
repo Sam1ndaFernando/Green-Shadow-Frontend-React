de3bc4663a77cfe8517039758e2a2abb38fc7e40
Saminda Fernando <uthpala2001@gmail.com>
2025-02-18 20:40:27 +0530

    store done

---
+++ src/store/store.ts
@@ -0,0 +1,20 @@
+import {configureStore} from "@reduxjs/toolkit"
+import StaffSlice from "../slices/StaffSlice.ts"
+import FieldSlice from "../slices/FieldSlice.ts";
+import CropSlice from "../slices/CropSlice.ts";
+import VehicleSlice from "../slices/VehicleSlice.ts";
+import LogSlice from "../slices/LogSlice.ts";
+import EquipmentSlice from "../slices/EquipmentSlice.ts";
+import UserSlice from "../slices/UserSlice.ts";
+
+export const store=configureStore({
+    reducer: {
+        staffSlice: StaffSlice,
+        fieldSlice: FieldSlice,
+        cropSlice:CropSlice,
+        vehicleSlice:VehicleSlice,
+        logSlice:LogSlice,
+        equipmentSlice:EquipmentSlice,
+        userSlice:UserSlice,
+    }
+})
